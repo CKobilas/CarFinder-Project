07530283a48ba31d3435cb298cb8186bc9305b79
Christian Kobilas <156360780+CKobilas@users.noreply.github.com>
2024-04-12 22:54:11 +0000

    v.04

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Define Allowed Vehicles List
-Allowed_Vehicles_List = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
+Allowed_Vehicles_List = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'Ram 1500']
 
 # Define function to print allowed vehicles
 def print_allowed_vehicles():
@@ -10,11 +10,12 @@ def print_allowed_vehicles():
 # Define function to display menu
 def display_menu():
   print("\n************************************\n\
-  AutoCountry Vehicle Finder v0.2\n************************************")
+  AutoCountry Vehicle Finder v0.4\n************************************")
   print("\n1. PRINT all Authorized Vehicles")
   print("2. SEARCH for Authorized Vehicle")
   print("3. ADD Authorized Vehicle")
-  print("4. Exit")
+  print("4. DELETE Authorized Vehicle")
+  print("5. Exit")
 
 # Define function to get user input on searched vehicle
 def search_vehicle():
@@ -32,6 +33,17 @@ def add_vehicle():
   else:
     Allowed_Vehicles_List.append(new_vehicle)
   return new_vehicle
+
+# Define function to delete vehicle from list
+def delete_vehicle():
+  delete_vehicle = input("\nPlease Enter the full Vehicle name you would like to REMOVE: ")
+  choice = input("\nAre you sure you want to remove " + delete_vehicle + " from the Authorized Vehicles list?")
+  if choice.lower() in ['yes', 'y']:
+    if delete_vehicle in Allowed_Vehicles_List:
+        Allowed_Vehicles_List.remove(delete_vehicle)
+        print("You have REMOVED " + delete_vehicle + " as an authorized vehicle")
+    else:
+        print(delete_vehicle, "is not in the authorized vehicles list.")
   
 # Define main function
 def main():
@@ -46,6 +58,8 @@ def main():
             new_vehicle = add_vehicle()
             print("\nYou have added " + new_vehicle + " as an authorized vehicle")
         elif choice == '4':
+            delete_vehicle()
+        elif choice == '5':
             print("\nThank you for using the AutoCountry Vehicle Finder, good-bye!")
             break
         else:
